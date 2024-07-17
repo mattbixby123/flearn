6be9ec80141d5fa9f2af15cb2bf957e7848db0f1
mattbixby123 <matthew.bixby1@gmail.com>
2024-07-17 18:37:52 -0400

    back end express router and api routes for prisma ORM db tables

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,11 @@
 const router = require("express").Router();
 
-// router.use("/[route]", require("./[componentFile]"));
+// router.use("/users", require("./users.js"));
+// router.use("/userProfiles", require("./userProfiles.js"));
+// router.use("/decks", require("./decks"));
+// router.use("/cards", require("./cards"));
+// router.use("/cardLists", require("./cardLists"));
+// router.use("/studySessions", require("./studySessions"));
+// router.use("/tags", require("./tags"));
 
 module.exports = router
